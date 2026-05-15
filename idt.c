03c54ab4184bbede46704557dfa6ed97fdcc065c
ELİTE <efe113878@gmail.com>
2026-05-16 02:44:39 +0300

    Create idt.c

---
+++ idt.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+
+struct idt_entry_struct {
+    uint16_t base_low;
+    uint16_t sel;
+    uint8_t  always0;
+    uint8_t  flags;
+    uint16_t base_high;
+} __attribute__((packed));
+
+struct idt_ptr_struct {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+struct idt_entry_struct idt_entries[256];
+struct idt_ptr_struct   idt_ptr;
+
+extern void load_idt(uint32_t);
+extern void keyboard_handler_asm(void);
+
+static inline void outb(uint16_t port, uint8_t val) {
+    __asm__ __volatile__("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+void set_idt_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags) {
+    idt_entries[num].base_low = base & 0xFFFF;
+    idt_entries[num].base_high = (base >> 16) & 0xFFFF;
+    idt_entries[num].sel     = sel;
+    idt_entries[num].always0 = 0;
+    idt_entries[num].flags   = flags;
+}
+
+void init_idt(void) {
+    idt_ptr.limit = (sizeof(struct idt_entry_struct) * 256) - 1;
+    idt_ptr.base  = (uint32_t)&idt_entries;
+
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20); 
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+
+    set_idt_gate(0x21, (uint32_t)keyboard_handler_asm, 0x08, 0x8E);
+    load_idt((uint32_t)&idt_ptr);
+}
