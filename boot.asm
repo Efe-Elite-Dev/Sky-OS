0d453181865dda692a0e059a36064aad00e3df23
ELİTE <efe113878@gmail.com>
2026-05-16 01:29:26 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,9 +1,9 @@
-; Sky-OS Saf Çekirdek Önyükleyici Tanımı
-MBALIGN  equ  1 << 0            ; Hizalama kuralları
-MEMINFO  equ  1 << 1            ; Bellek haritasını oku
-FLAGS    equ  MBALIGN | MEMINFO ; Multiboot bayrakları
-MAGIC    equ  0x1BADB002        ; İşlemcinin tanıyacağı sihirli numara
-CHECKSUM equ -(MAGIC + FLAGS)   ; Kontrol toplamı
+cat << 'EOF' > boot.asm
+MBALIGN  equ  1 << 0            
+MEMINFO  equ  1 << 1            
+FLAGS    equ  MBALIGN | MEMINFO 
+MAGIC    equ  0x1BADB002        
+CHECKSUM equ -(MAGIC + FLAGS)   
 
 section .multiboot
 align 4
@@ -14,7 +14,7 @@ align 4
 section .bootstrap_stack, nobits
 align 16
 stack_bottom:
-    resb 16384 ; Çekirdek için 16 KB'lık saf RAM alanı ayırıyoruz
+    resb 16384 
 stack_top:
 
 section .text
@@ -23,9 +23,10 @@ extern kernel_main
 
 _start:
     mov esp, stack_top
-    call kernel_main ; İşte burası! Bizim C dilindeki ana motora zıplıyoruz.
+    call kernel_main
 
     cli
 .hang:
     hlt
     jmp .hang
+EOF
