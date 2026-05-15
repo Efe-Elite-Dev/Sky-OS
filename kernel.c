719bd5a5f2580f6d8399836564d6a5e79d82d45c
ELİTE <efe113878@gmail.com>
2026-05-16 02:43:59 +0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,6 +1,22 @@
 #include "screen.h"
 #include "keyboard.h"
-#include "idt.h"
+#include <stdint.h>
+
+// idt.h icerigini dogrudan buraya gomduk, harici dosya arama derdi bitti!
+struct idt_entry_struct {
+    uint16_t base_low;
+    uint16_t sel;
+    uint8_t  always0;
+    uint8_t  flags;
+    uint16_t base_high;
+} __attribute__((packed));
+
+struct idt_ptr_struct {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+extern void init_idt(void);
 
 void kernel_main(void) {
     sky_clear_screen();
