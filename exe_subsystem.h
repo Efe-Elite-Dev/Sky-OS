2851330f05af5199a36a7b7794752e69cb8088ee
ELİTE <efe113878@gmail.com>
2026-05-16 12:54:35 +0300

    Update exe_subsystem.h

--- exe_subsystem.h
+++ exe_subsystem.h
@@ -3,12 +3,8 @@
 
 #include <stdint.h>
 
-static inline void run_exe_subsystem(int win_x, int win_y) {
-    gui_draw_rect(win_x + 15, win_y + 45, 470, 290, 0xF3F3F3);
-    gui_draw_rect(win_x + 50, win_y + 80, 22, 22, 0x0078D4); 
-    gui_draw_rect(win_x + 75, win_y + 80, 22, 22, 0x0078D4); 
-    gui_draw_rect(win_x + 50, win_y + 105, 22, 22, 0x0078D4); 
-    gui_draw_rect(win_x + 75, win_y + 105, 22, 22, 0x0078D4); 
-}
+void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
+
+void run_exe_subsystem(int win_x, int win_y);
 
 #endif
