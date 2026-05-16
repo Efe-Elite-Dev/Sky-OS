f047c912ce6221b0fb9e13abbd01706e675d3366
ELİTE <efe113878@gmail.com>
2026-05-16 14:51:11 +0300

    Create mouse.h

---
+++ mouse.h
@@ -0,0 +1,21 @@
+#ifndef MOUSE_H
+#define MOUSE_H
+
+#include <stdint.h>
+
+// Ekran sınırlarını kontrol etmek için kernel çözünürlükleri
+#define SCREEN_WIDTH     800
+#define SCREEN_HEIGHT    600
+
+// Dışarıya aktarılan global fare koordinatları
+extern int mouse_x;
+extern int mouse_y;
+
+// Fonksiyon prototipleri
+void init_mouse(void);
+void handle_mouse_polling(void);
+
+// Fare okunun 12x19 matrisi (Sprite)
+extern const uint8_t mouse_pointer_sprite[19][12];
+
+#endif
