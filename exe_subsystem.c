2868c7451f31d45516a0eaa633054ae68b55a039
ELİTE <efe113878@gmail.com>
2026-05-16 12:55:02 +0300

    Create exe_subsystem.c

---
+++ exe_subsystem.c
@@ -0,0 +1,9 @@
+#include "exe_subsystem.h"
+
+void run_exe_subsystem(int win_x, int win_y) {
+    gui_draw_rect(win_x + 15, win_y + 45, 470, 290, 0xF3F3F3);
+    gui_draw_rect(win_x + 50, win_y + 80, 22, 22, 0x0078D4); 
+    gui_draw_rect(win_x + 75, win_y + 80, 22, 22, 0x0078D4); 
+    gui_draw_rect(win_x + 50, win_y + 105, 22, 22, 0x0078D4); 
+    gui_draw_rect(win_x + 75, win_y + 105, 22, 22, 0x0078D4); 
+}
